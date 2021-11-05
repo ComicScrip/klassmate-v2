d6a00fea46707009c6e47136922ec4f1f2a041c3
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-11-05 10:36:50 +0100

    notes

--- src/KlassmateAPI.js
+++ src/KlassmateAPI.js
@@ -4,4 +4,8 @@ const instance = axios.create({
   baseURL: process.env.REACT_APP_KLASSMATE_API_BASE_URL,
 });
 
+export const getNote = (id) => {
+  return instance.get(`/notes/${id}`).then((res) => res.data);
+};
+
 export default instance;
--- src/screens/EditNotePage.js
+++ src/screens/EditNotePage.js
@@ -1,3 +1,38 @@
+import { useState } from 'react';
+
+import KlassmateAPI from '../KlassmateAPI';
+
 export default function EditNotePage() {
-  return <div>Edit not page</div>;
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleNoteSubmit = (e) => {
+    e.preventDefault();
+    KlassmateAPI.post('/notes', { title, content });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleNoteSubmit}>
+        <label htmlFor="note-title">
+          Title :
+          <input
+            value={title}
+            id="note-title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+
+        <label htmlFor="note-content">
+          Content :
+          <textarea
+            id="note-content"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </label>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 }
